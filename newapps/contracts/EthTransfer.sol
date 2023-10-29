d497d7889d22e1cf23640fa81ff9976f714b0b3d
Loy <tanzenchai0923@hotmail.com>
2023-10-29 23:50:26 +0800

    third push

--- newapps/contracts/EthTransfer.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-contract EthTransfer {
-    function transfer(address payable to) external payable {
-        require(msg.value > 0, "Must send Ether");
-        to.transfer(msg.value);
-    }
-}
